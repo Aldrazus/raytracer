70911835720cc093fca693fe47f6958ca0b78b34
Aldrazus <aespinal59@gmail.com>
2023-04-13 08:16:55 -0400

    add za warudo

---
+++ src/hittable.rs
@@ -0,0 +1,105 @@
+use crate::{ray::Ray, Point3, Vec3};
+use std::rc::Rc;
+
+#[derive(Clone, Copy)]
+pub struct HitRecord {
+    pub p: Point3,
+    pub normal: Vec3,
+    pub t: f64,
+    pub front_face: bool,
+}
+
+impl HitRecord {
+    pub fn new() -> Self {
+        HitRecord {
+            p: Vec3(0., 0., 0.),
+            normal: Vec3(0., 0., 0.),
+            t: 0.,
+            front_face: false,
+        }
+    }
+
+    #[inline]
+    pub fn set_face_normal(&mut self, r: &Ray, outward_normal: &Vec3) {
+        self.front_face = Vec3::dot(r.direction, *outward_normal) < 0.;
+        self.normal = if self.front_face {
+            *outward_normal
+        } else {
+            -*outward_normal
+        };
+    }
+}
+
+pub trait Hittable {
+    fn hit(&self, r: &Ray, t_min: f64, t_max: f64, rec: &mut HitRecord) -> bool;
+}
+
+pub struct Sphere {
+    pub center: Point3,
+    pub radius: f64,
+}
+
+impl Hittable for Sphere {
+    fn hit(&self, r: &Ray, t_min: f64, t_max: f64, rec: &mut HitRecord) -> bool {
+        let oc = r.origin - self.center;
+        let a = r.direction.length_squared();
+        let half_b = Vec3::dot(oc, r.direction);
+        let c = oc.length_squared() - self.radius * self.radius;
+        let discriminant = half_b * half_b - a * c;
+
+        if discriminant < 0. {
+            return false;
+        }
+        let sqrtd = f64::sqrt(discriminant);
+
+        let mut root = (-half_b - sqrtd) / a;
+        if root < t_min || root > t_max {
+            root = (-half_b + sqrtd) / a;
+            if root < t_min || root > t_max {
+                return false;
+            }
+        }
+
+        rec.t = root;
+        rec.p = r.at(rec.t);
+        let outward_normal = (rec.p - self.center) / self.radius;
+        rec.set_face_normal(r, &outward_normal);
+        true
+    }
+}
+
+pub struct HittableList {
+    objects: Vec<Rc<dyn Hittable>>,
+}
+
+impl HittableList {
+    pub fn new() -> Self {
+        HittableList { objects: vec![] }
+    }
+
+    pub fn clear(&mut self) {
+        self.objects.clear();
+    }
+
+    pub fn add(&mut self, object: Rc<dyn Hittable>) {
+        self.objects.push(object);
+    }
+}
+
+impl Hittable for HittableList {
+    fn hit(&self, r: &Ray, t_min: f64, t_max: f64, rec: &mut HitRecord) -> bool {
+        let mut temp_rec = HitRecord::new();
+        let mut hit_anything = false;
+        let mut closest_so_far = t_max;
+
+        for object in &self.objects {
+            if object.hit(r, t_min, closest_so_far, &mut temp_rec) {
+                hit_anything = true;
+                closest_so_far = temp_rec.t;
+                *rec = temp_rec;
+            }
+        }
+
+        hit_anything
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,46 @@
+use std::rc::Rc;
+
+use hittable::{HitRecord, Hittable};
 use ray::Ray;
 
-use crate::vec3::{write_color, Color, Point3, Vec3};
+use crate::{
+    hittable::{HittableList, Sphere},
+    vec3::{write_color, Color, Point3, Vec3},
+};
 
+mod hittable;
 mod ray;
+mod util;
 mod vec3;
 
-fn ray_color(r: &Ray) -> Color {
-    if let Some(t) = hit_sphere(&Vec3(0., 0., -1.), 0.5, r) {
-        let normal = Vec3::unit_vector(r.at(t) - Vec3(0., 0., -1.));
-        return 0.5 * Vec3(normal.x() + 1., normal.y() + 1., normal.z() + 1.);
+fn ray_color(r: &Ray, world: &dyn Hittable) -> Color {
+    let mut rec = HitRecord::new();
+    if world.hit(r, 0., f64::INFINITY, &mut rec) {
+        return 0.5 * (rec.normal + Vec3(1., 1., 1.));
     }
     let unit_direction = Vec3::unit_vector(r.direction);
     let t = 0.5 * (unit_direction.y() + 1.0);
     (1.0 - t) * Vec3(1.0, 1.0, 1.0) + t * Vec3(0.5, 0.7, 1.0)
 }
 
-fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> Option<f64> {
-    let oc = r.origin - *center;
-    let a = r.direction.length_squared();
-    let half_b = Vec3::dot(oc, r.direction);
-    let c = oc.length_squared() - radius * radius;
-    let discriminant = half_b * half_b - a * c;
-    
-    if discriminant < 0. {
-        None
-    } else {
-        Some((-half_b - f64::sqrt(discriminant)) / a)
-    }
-}
-
 fn main() {
+    // Image
     let aspect_ratio = 16.0 / 9.0;
     let image_width = 400;
     let image_height = (image_width as f64 / aspect_ratio) as i32;
 
+    // World
+    let mut world = HittableList::new();
+    world.add(Rc::new(Sphere {
+        center: Vec3(0., 0., -1.),
+        radius: 0.5,
+    }));
+    world.add(Rc::new(Sphere {
+        center: Vec3(0., -100.5, -1.),
+        radius: 100.,
+    }));
+
+    // Camera
     let viewport_height = 2.0;
     let viewport_width = aspect_ratio * viewport_height;
     let focal_length = 1.0;
@@ -43,6 +50,7 @@ fn main() {
     let vertical = Vec3(0., viewport_height, 0.);
     let lower_left_corner = origin - horizontal / 2. - vertical / 2. - Vec3(0., 0., focal_length);
 
+    // Render
     println!("P3\n{} {}\n255", image_width, image_height);
 
     for j in (0..image_height).rev() {
@@ -54,7 +62,7 @@ fn main() {
                 origin,
                 lower_left_corner + u * horizontal + v * vertical - origin,
             );
-            let pixel_color = ray_color(&r);
+            let pixel_color = ray_color(&r, &world);
             write_color(pixel_color);
         }
     }
